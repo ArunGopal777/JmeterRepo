9127489dc439b284544b725f59b73c7754675bac
ArunGopal777 <44263122+ArunGopal777@users.noreply.github.com>
2018-10-18 17:53:43 +0530

    Groovy script by Arun Gopal

---
+++ JmeterScript.groovy
@@ -0,0 +1,7 @@
+node {
+
+stage 'Run JMeter Test'
+   
+bat 'C:/Users/C54509A/Desktop/Arun_Gopal_C54509A/Tools_Library/ApacheJMeter/apache-jmeter-5.0Arun/apache-jmeter-5.0/bin/jmeter.bat -n -t C:/Users/C54509A/Desktop/Sample_Automation_Jmeter_Script.jmx'
+
+}
